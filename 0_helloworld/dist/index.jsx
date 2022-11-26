929ebf5a3bc7b6b9a437806a458dcbcee0292464
Maru <marumaruwei@gmail.com>
2022-11-26 23:40:27 +0800

    table

--- 0_helloworld/dist/index.jsx
+++ 0_helloworld/dist/index.jsx
@@ -4,10 +4,13 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 //ReactDOM.render(<div>"HELLO REACT"</div>, document.getElementById('root'));
 import App from '../src/App.jsx';
+import Table from '../src/Table.jsx';
+
  ReactDOM.render(
   // <h1>hello, world!!!!!!</h1>,
    <React.StrictMode>
      <App />
+     <Table />
    </React.StrictMode>,
    document.getElementById('root'),
  );
