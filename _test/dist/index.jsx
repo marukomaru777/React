632426df9ef209f82e92938c51fe84c7248f0566
Maru <marumaruwei@gmail.com>
2022-12-02 02:15:05 +0800

    delete files

--- _test/dist/index.jsx
+++ _test/dist/index.jsx
@@ -1,16 +1,14 @@
-/* Add JavaScript code here! */
-//console.log('Hello World! You did it! Welcome to Snowpack :D');
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-//ReactDOM.render(<div>"HELLO REACT"</div>, document.getElementById('root'));
 import App from '../src/App.jsx';
-import Table from '../src/Table.jsx';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
  ReactDOM.render(
-  // <h1>hello, world!!!!!!</h1>,
    <React.StrictMode>
      <App />
-     <Table />
    </React.StrictMode>,
    document.getElementById('root'),
  );
--- _test/src/App.jsx
+++ _test/src/App.jsx
@@ -1,26 +1,86 @@
-import React, {useState, useEffect} from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//import {useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BTable from 'react-bootstrap/Table';
+import BButton from 'react-bootstrap/Button';
+import { useTable } from 'react-table';
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Update the count (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        </p>
-      </header>
-    </div>
+  // Use the state and functions returned from useTable to build your UI
+  const data = React.useMemo(
+    () => [
+      {
+        col1: '標的',
+        col2: '2330.TW',
+      },
+      {
+        col1: '起始時間',
+        col2: '2022-01-01',
+      }
+    ],[]
   );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: '表頭1',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: '表頭2',
+        accessor: 'col2',
+      }
+    ],[]
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+  // const tableInstance = useTable({ columns, data })  // Render the UI for your table
+  // const {
+  //   getTableProps,
+  //   getTableBodyProps,
+  //   headerGroups,
+  //   rows,
+  //   prepareRow,
+  // } = tableInstance
+
+  return (
+    <BTable striped bordered hover size="sm" {...getTableProps()}  
+            style={{ 
+              width: '20%',
+              marginTop: '30px',
+              marginLeft: '20px'
+            }}>
+      {/* <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead> */}
+      
+      <tbody {...getTableBodyProps()} >
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan='2' align='center'>
+            <BButton>查詢</BButton>
+          </td>
+        </tr>
+      </tfoot>
+    </BTable>
+  )
 }
 
-export default App;
+export default App
