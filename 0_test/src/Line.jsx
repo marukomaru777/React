087a3311723b0b0109776753af7ce1ed4e928f10
Maru <marumaruwei@gmail.com>
2022-12-05 00:20:38 +0800

    調整圖

--- 0_test/src/Line.jsx
+++ 0_test/src/Line.jsx
@@ -64,34 +64,42 @@ function Line() {
         {
           name: "Open",
           data: [150, 230, 224, 218, 135, 147],
-          type: "bar",
+          yAxisIndex: 1,
+          type: "line",
+          smooth: true,
         },
         {
           name: "Low",
           data: [150, 230, 224, 218, 135, 147],
-          type: "bar",
+          yAxisIndex: 1,
+          type: "line",
+          smooth: true,
         },
         {
           name: "High",
           data: [150, 230, 224, 218, 135, 147],
-          type: "bar",
+          yAxisIndex: 1,
+          type: "line",
+          smooth: true,
         },
         {
           name: "Close",
           data: [880, 30, 124, 118, 35, 47],
-          type: "bar",
+          yAxisIndex: 1,
+          type: "line",
+          smooth: true,
         },
         {
           name: "Adj Close",
           data: [660, 30, 124, 118, 35, 47],
-          type: "bar",
+          yAxisIndex: 1,
+          type: "line",
+          smooth: true,
         },
         {
           name: "Volumn",
           data: [880, 30, 124, 118, 35, 47],
-          yAxisIndex: 1,
-          type: "line",
-          smooth: true,
+          type: "bar",
         },
       ],
     };
@@ -100,7 +108,7 @@ function Line() {
 
   return (
     <div style={{ textAlign: "center" }}>
-      <h2>React Echarts 折线+柱状图</h2>
+      <h2>2330.TW</h2>
       <div ref={chartRef} style={{ height: "400px" }}></div>
     </div>
   );
