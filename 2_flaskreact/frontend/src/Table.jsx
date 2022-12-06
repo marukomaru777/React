6e98681db873da13e1950cf80fe787b51fccf353
Maru <marumaruwei@gmail.com>
2022-12-06 21:20:02 +0800

    ajax

--- 2_flaskreact/frontend/src/Table.jsx
+++ 2_flaskreact/frontend/src/Table.jsx
@@ -3,30 +3,47 @@ import React from 'react'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import BTable from 'react-bootstrap/Table';
 import BButton from 'react-bootstrap/Button';
+import $ from 'jquery';
 
 function Table() {
+  let getStockData =function(e){
+    $.ajax({
+      url:"http://192.168.168.14:8080",
+      type: "GET",
+      data:{
+        stock: $('#stock').val,
+        dateStr: $('#dateStr').val,
+      },
+      success: function(result){
+        debugger
+        console.log(result);
+      },
+      error: function(error){
+        debugger
+        console.log(error)
+      }
+    });
+  }
   return (
-    <form  method="get" action="result">
-      <BTable striped bordered hover size="sm">
-        <tbody>
-          <tr>
-              <td>標的</td>
-              <td><input type="text" name="stock" value="2330.TW"></input></td>
-          </tr>
-          <tr>
-              <td>起始時間</td>
-              <td><input type="text" name="dateStr" value="2022-01-01"></input></td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan='2' align='center'>
-            <input class="btn btn-primary" type="submit" value="查詢"></input>
-            </td>
-          </tr>
-        </tfoot>
-      </BTable>
-    </form>
+    <BTable striped bordered hover size="sm">
+      <tbody>
+        <tr>
+            <td>標的</td>
+            <td><input type="text" id="stock" name="stock" value="2330.TW"></input></td>
+        </tr>
+        <tr>
+            <td>起始時間</td>
+            <td><input type="text" id="dateStr" name="dateStr" value="2022-01-01"></input></td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan='2' align='center'>
+          <BButton class="btn btn-primary" onClick={getStockData}>查詢</BButton>
+          </td>
+        </tr>
+      </tfoot>
+    </BTable>
   )
 }
 
